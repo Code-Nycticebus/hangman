3807496039455e52edaffad765169a3c67fdede8
Loris Kriyonas <loris.kriyonas@gmail.com>
2023-10-26 17:59:57 +0200

    added language support

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from random import choice
 
+WORD_TEXT_FILE = "res/words_DE.txt"
+
 def load_word_list() -> list[str]:
     word_list: list[str] = []
-    with open("res/words.txt", "r") as f:
+    with open(WORD_TEXT_FILE, "r") as f:
         for line in f.readlines():
             word_list.append(line.strip().upper())
     return word_list 
